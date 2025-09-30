4c440faab0f6d39190d28f3c8609292227b686ad
rafaelmp0 <fali10ps@gmail.com>
2025-09-30 19:54:30 +0200

    update

--- DAM/T1/ProyectoInicial/src/Main.java
+++ DAM/T1/ProyectoInicial/src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hola este es mi primer programa en Java");
+        System.out.println("Segunda línea");
+
+    }
+
 }
